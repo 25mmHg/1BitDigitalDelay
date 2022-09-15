cfc9d079d130a5a38264eb4b2b400b5420574b1a
25mmHg <23mmhg@gmail.com>
2022-09-15 17:26:49 +0200

    Update 1BitDigitalDelay.ino

--- 1BitDigitalDelay.ino
+++ 1BitDigitalDelay.ino
@@ -1,32 +1,42 @@
-/*******************Digitaldelay for DRUM trigger***********************
-If the INPUT got a LOW edge, the code generate an 1ms trigger event in 
+/*******************Digitaldelay V1.05 for DRUM trigger******************
+  If the INPUT got a LOW edge, the code generate an 1ms trigger event in
      realtime and also a second 1ms trigger event with delay.
       The delayTime is adjusted using the ATmega32U4-USB-interface.
-               The ringbuffer holds 512 trigger events. 
+               The ringbuffer holds 512 trigger events.
                  The maximum delay is limited by 60s.
                       The resolution is 1ms.
                            by 25mmHg
                            Have fun!
-
-                         Version 1.02
+                          Version 1.05
+                        heartbeat mode
+                          Version 1.04
+                 new pins (matched with PCB)
+                          Version 1.03
+          add LED-monitor output and change PORTS
+                          Version 1.02
                     faster port operations
-                samplingrate ca.100kHz possible                        
-                         Version 1.01
+                samplingrate ca.100kHz possible
+                          Version 1.01
             button debounce logic is much faster (5ms)
-                         Version 1.00
+                          Version 1.00
                 first working needs optimation
  ***********************************************************************/
 
 #define MAXDELAYTIME 60000
-#define MAXARRAYSIZE 512
-#define BOUNCETIME 5
+#define MAXARRAYSIZE 256
+#define BOUNCETIME 20
+#define FLASHTIME 40
 
-#define INBUTTON PB2 //MOSI
-#define OUTDELAY PB3 //MISO
-#define OUTNOOOW PC7 //D13
-#define DEBUGPIN PD6 //D12
+#define INBUTTON PF4 //A3
+#define OUTDELAY PF7 //A0
+//Demo only
+//#define OUTDELAY PF5 //A2
+#define OUTNOOOW PF6 //A1
+#define OUTCOOPY PC7 //D13
+#define DEBUGPIN PB6 //A10
 #define DEBUGMODE
 #define SERIALDEBUG
+//#define HEARTBEAT
 
 //global variables related to time
 unsigned long oldMillis = 0;
@@ -50,19 +60,24 @@ void initUSB2serial() {
   Serial.begin(115200);
   while (!Serial);
   delay(2000);
-  Serial.println(F("Hello. I generate Delays for you."));
-  Serial.print(F("Please tell me a new delayTime fom 0 to "));
+  Serial.println(F("1BitDigitalDelay with 1ms resolution (Version 1.05)"));
+  Serial.print(F("Add a new delayTime fom 0 to "));
   Serial.print(maxDelayTime);
   Serial.println(F("ms"));
+#ifdef SERIALDEBUG
+  Serial.println(F("use SPACEBAR for array view"));
+#endif
 }
 
 void initPins() {
-  DDRB &= ~(1 << INBUTTON);
-  PORTB |= (1 << INBUTTON);
-  DDRB |=  (1 << OUTDELAY);
-  DDRC |=  (1 << OUTNOOOW);
+  DDRF &= ~(1 << INBUTTON);
+  // pullups installed on PCB
+  // PORTF |= (1 << INBUTTON);
+  DDRF |=  (1 << OUTDELAY);
+  DDRF |=  (1 << OUTNOOOW);
 #ifdef DEBUGMODE
-  DDRD |=  (1 << DEBUGPIN);
+  DDRB |=  (1 << DEBUGPIN);
+  DDRC |=  (1 << OUTCOOPY);
 #endif
 }
 
@@ -78,7 +93,7 @@ void setNewDelayTime() {
       val = val * 10 + ch - '0';
     }
 #ifdef SERIALDEBUG
-    // Look 4 SPACE
+    // Look 4 SPACEBAR
     else if (ch == 32) {
       val = delayTime;
       Serial.println("Array:");
@@ -94,7 +109,7 @@ void setNewDelayTime() {
     else if (ch == 10) {
       if (val <= maxDelayTime) {
         delayTime = val;
-        Serial.print(val);
+        Serial.print(delayTime);
         Serial.println(F("ms"));
       }
       else
@@ -104,11 +119,23 @@ void setNewDelayTime() {
 }
 
 void triggerNow(boolean val) {
-  val ? PORTC |= (1 << OUTNOOOW) : PORTC &= ~(1 << OUTNOOOW);
+  val ? PORTF |= (1 << OUTNOOOW) : PORTF &= ~(1 << OUTNOOOW);
 }
 
 void triggerDel(boolean val) {
-  val ? PORTB |= (1 << OUTDELAY) : PORTB &= ~(1 << OUTDELAY);
+  val ? PORTF |= (1 << OUTDELAY) : PORTF &= ~(1 << OUTDELAY);
+}
+
+void flashLED() {
+  static byte flashTimer;
+  if (PORTF & (1 << OUTDELAY | 1 << OUTNOOOW) || (0 < flashTimer  && flashTimer < FLASHTIME)) {
+    PORTC  |= (1 << OUTCOOPY);
+    flashTimer > 0 ? flashTimer-- : 0;
+  }
+  else {
+    PORTC &= ~(1 << OUTCOOPY);
+    flashTimer = FLASHTIME;
+  }
 }
 
 boolean getButton() {
@@ -116,13 +143,13 @@ boolean getButton() {
   static boolean lastButton;
   static boolean newButton;
   lastButton = newButton;
-  newButton = !(PINB & (1 << INBUTTON));
+  newButton = !(PINF & (1 << INBUTTON));
   if (newButton  && !lastButton && counter == 0) {
     counter = BOUNCETIME;
     return (true);
   }
   else {
-    counter > 0 ? counter-- : counter = 0;
+    counter > 0 ? counter-- :  counter = 0;
     return (false);
   }
 }
@@ -130,7 +157,7 @@ boolean getButton() {
 boolean writeTimeStamps() {
   if (timeStamps[writeCursor] == 0xFFFFFFFF) {
     timeStamps[writeCursor] = valMillis + delayTime;
-    writeCursor < maxArrayCount ? writeCursor++ : writeCursor = 0;
+    writeCursor < maxArrayCount ? writeCursor++ : writeCursor =  0;
     return (true);
   }
   else
@@ -140,7 +167,7 @@ boolean writeTimeStamps() {
 boolean readTimeStamps() {
   if (valMillis >= timeStamps[readCursor]) {
     timeStamps[readCursor] = 0xFFFFFFFF;
-    readCursor < maxArrayCount ? readCursor++ : readCursor = 0;
+    readCursor < maxArrayCount ? readCursor++ :  readCursor = 0;
     return (true);
   }
   else
@@ -159,7 +186,7 @@ void loop() {
 
 #ifdef DEBUGMODE
     // Use Logic Analyzer
-    PORTD |= (1 << DEBUGPIN);
+    PORTB |= (1 << DEBUGPIN);
 #endif
 
     oldMillis = valMillis;
@@ -167,6 +194,17 @@ void loop() {
     // put your code here, to run every millisecond
     // adjust delay
     setNewDelayTime();
+
+#ifdef HEARTBEAT
+    // wait 4 heartbeat
+    if ((valMillis & 0xFFFUL) == 0) {
+      if (writeTimeStamps()) {
+        triggerNow(true);
+      }
+      else
+        bufferOverRun();
+    }
+#else
     // Test 4 Button
     if (getButton()) {
       if (writeTimeStamps()) {
@@ -175,6 +213,8 @@ void loop() {
       else
         bufferOverRun();
     }
+#endif
+
     else triggerNow(false);
     // Test 4 Delay
     triggerDel(readTimeStamps());
@@ -183,12 +223,13 @@ void loop() {
 
 #ifdef DEBUGMODE
     // Use Logic Analyzer
-    PORTD &= ~(1 << DEBUGPIN);
+    PORTB &= ~(1 << DEBUGPIN);
+    flashLED();
 #endif
 
   }
   else if (oldMillis > valMillis) {
-    // put your code here, to deal with timer overflow, but this happens in 50days
+    // put your code here, to deal with timer overflow in 50days ;-)
     ;
   }
   else {
